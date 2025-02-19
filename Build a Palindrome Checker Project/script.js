cb23b88ab80a154ff0e2dfd355f126cc83959902
Joe Ma <hl.ma@outlook.com>
2025-02-19 13:24:49 +1100

    Build a Palindrome Checker Project

---
+++ Build a Palindrome Checker Project/script.js
@@ -0,0 +1,30 @@
+const userInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const displayResult = document.getElementById("result");
+
+const isPalindrome = (str) => {
+    const cleanInput = str.split('')
+        .filter(char => /[a-zA-Z0-9]/.test(char))
+        .join('')
+        .toLowerCase();
+
+    const reverseString = cleanInput.split('').reverse().join('');
+    return cleanInput === reverseString;
+}
+
+const checkPalindrome = (e) => {
+    if (e.type === "click" || e.key === "Enter") {
+        const input = userInput.value;
+
+        if (input === '')
+            return alert("Please input a value");
+
+        const result = `${input} ${isPalindrome(input) ? "is" : "is not"} a palindrome.`;
+        displayResult.textContent = result;
+        displayResult.classList.remove("hidden");
+        userInput.value = "";
+    };
+};
+
+userInput.addEventListener("keydown", checkPalindrome);
+checkButton.addEventListener("click", checkPalindrome);
