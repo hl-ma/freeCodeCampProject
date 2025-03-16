dba1d3d599071bd2506ecd68d8e790dabca4be6b
Joe Ma <hl.ma@outlook.com>
2025-03-17 02:35:10 +1100

    Build a Cash Register

---
+++ Build a Cash Register/scriptNormal.js
@@ -0,0 +1,148 @@
+let price = 1.87;
+let cid = [
+    ['PENNY', 1.01],
+    ['NICKEL', 2.05],
+    ['DIME', 3.1],
+    ['QUARTER', 4.25],
+    ['ONE', 90],
+    ['FIVE', 55],
+    ['TEN', 20],
+    ['TWENTY', 60],
+    ['ONE HUNDRED', 100]
+];
+
+// let price = 19.5;
+// let cid = [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]];
+
+
+const cashInput = document.getElementById('cash');
+const purchaseBtn = document.getElementById('purchase-btn');
+const changeDueDisplay = document.getElementById('change-due');
+const registerDisplay = document.querySelector('.top-display');
+const registerDrawer = document.querySelector('.drawer');
+
+const updateChangeDueDisplay = (expression, status, change) => {
+    changeDueDisplay.style.display = 'block';
+    switch (expression) {
+        case "exactCash":
+            changeDueDisplay.textContent = 'No change due - customer paid with exact cash';
+            break;
+        case "customerInsufficientCash":
+            alert("Customer does not have enough money to purchase the item");
+            changeDueDisplay.textContent = 'Customer does not have enough money to purchase the item';
+            break;
+        case "changeDue":
+            changeDueDisplay.innerHTML = `
+            <p>Status: ${status}<p> 
+            ${change.map((el) => `
+                <p>${el[0]}: $${el[1].toFixed(2)}</p>
+                `).join('')}`;
+            break;
+        case "RegisterInsufficientFunds":
+            changeDueDisplay.innerHTML = `<p>Status: ${status}<p>`;
+            break;
+        default:
+            changeDueDisplay.textContent = 'Error: Undefined expression';
+    }
+}
+
+const checkCashRegister = () => {
+    const priceToCents = Math.round(price * 100);
+    const cashToCents = Math.round(Number(cashInput.value) * 100);
+    const cidToCents = Math.round(cid.reduce((acc, curr) => acc + curr[1], 0) * 100);
+    let changeDue = cashToCents - priceToCents;
+
+    if (cashToCents === priceToCents) return { expression: 'exactCash', status: '', change: [] };
+
+    if (cashToCents < priceToCents) return { expression: 'customerInsufficientCash', status: '', change: [] };
+
+    if (changeDue > cidToCents) return { expression: 'RegisterInsufficientFunds', status: 'INSUFFICIENT_FUNDS', change: [] };
+
+    const currencyUnits = [
+        ['ONE HUNDRED', 10000],
+        ['TWENTY', 2000],
+        ['TEN', 1000],
+        ['FIVE', 500],
+        ['ONE', 100],
+        ['QUARTER', 25],
+        ['DIME', 10],
+        ['NICKEL', 5],
+        ['PENNY', 1]
+    ];
+
+    const changeToGive = [];
+    currencyUnits.map(([currency, unitValue]) => {
+        let currencyInDrawerToCents = Math.round(
+            cid.find(el => el[0] === currency)[1] * 100
+        );
+        let currencyUsed = 0;
+
+        while (changeDue >= unitValue && currencyInDrawerToCents > 0) {
+            currencyInDrawerToCents -= unitValue;
+            changeDue -= unitValue;
+            currencyUsed++;
+        }
+        if (currencyUsed) {
+            changeToGive.unshift([currency, currencyUsed * unitValue / 100]);
+        }
+    })
+
+    if (changeDue !== 0) return { expression: 'RegisterInsufficientFunds', status: 'INSUFFICIENT_FUNDS', change: [] };
+
+    let cidRemaining = cidToCents;
+    changeToGive.map(([currency, value]) => {
+        for (let cidCurrency of cid) {
+            if (cidCurrency[0] === currency) {
+                cidCurrency[1] -= value;
+                cidRemaining -= value * 100;
+                break;
+            }
+        }
+    })
+    return cidRemaining === 0
+        ? { expression: 'changeDue', status: 'CLOSED', change: changeToGive }
+        : { expression: 'changeDue', status: 'OPEN', change: changeToGive };
+}
+
+const updateRegisterDisplay = () => {
+    const currencyNameMap = {
+        PENNY: 'Pennies',
+        NICKEL: 'Nickels',
+        DIME: 'Dimes',
+        QUARTER: 'Quarters',
+        ONE: 'Ones',
+        FIVE: 'Fives',
+        TEN: 'Tens',
+        TWENTY: 'Twenties',
+        'ONE HUNDRED': 'Hundreds'
+    };
+
+    const drawerInfo = cid.map(
+        (el) => currencyNameMap[el[0]] + ': ' + el[1].toFixed(2)
+    ).join('<br>');
+
+    registerDisplay.textContent = `Total: $${price.toFixed(2)}`;
+    registerDrawer.innerHTML = `
+            <strong>Change in drawer:</strong><br> 
+            ${drawerInfo}
+        `;
+}
+
+
+purchaseBtn.addEventListener('click', () => {
+    const { expression, status, change } = checkCashRegister();
+    updateChangeDueDisplay(expression, status, change);
+    updateRegisterDisplay();
+})
+
+cashInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        const { expression, status, change } = checkCashRegister();
+        updateChangeDueDisplay(expression, status, change);
+        updateRegisterDisplay();
+    }
+})
+
+window.addEventListener('load', () => {
+    updateRegisterDisplay();
+});
